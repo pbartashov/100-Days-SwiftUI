31e80e6143910112781262bc7f23da7a2efd2340
pbartashov <p.bartashov@icloud.com>
2024-09-17 11:04:00 +0930

    GuessTheFlag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,83 +9,133 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var a = 1
+    private let maxAttemptCount = 8
+    @State private var attemptCount = 0
 
-    @State var showingAlert = false
-    @State var showingAlert2 = false
+    @State private var showingCurrentScore = false
+    @State private var showingFinalScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+
+    @State private var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Spain",
+        "UK",
+        "Ukraine",
+        "US"
+    ].shuffled()
+
+    @State private var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-//        VStack {
-//            ForEach(0..<3) { _ in
-//                HStack {
-//                    ForEach(0..<3) { _ in
-//                        Text("Hello, world!")
-//                    }
-//                }
-//            }
-//        }
         ZStack {
+            RadialGradient(
+                stops: [
+                    .init(
+                        color: Color(
+                            red: 0.1, green: 0.2, blue: 0.45
+                        ),
+                        location: 0.3
+                    ),
+                    .init(
+                        color: Color(
+                            red: 0.76, green: 0.15, blue: 0.26
+                        ),
+                        location: 0.3
+                    ),
+                ],
+                center: .top,
+                startRadius: 200,
+                endRadius: 400
+            ).ignoresSafeArea()
+
             VStack {
-                Button("Button 1") { }
-                    .buttonStyle(.bordered)
-                Button("Button 2", role: .destructive) { }
-                    .buttonStyle(.bordered)
-                Button("Button 3") { }
-                    .tint(.mint)
-                    .buttonStyle(.borderedProminent)
-                Button("Button 4", role: .destructive) { }
-                    .buttonStyle(.borderedProminent)
-                Button {
-                   a += 1
-                } label: {
-                    Text("Tap me!")
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background(.red)
-                    Text("Now")
-                }
+                Spacer()
+                
+                Text("Guess the flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
 
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Image(systemName: "pencil")
-                }
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
 
-                Button("Edit", systemImage: "pencil") {
-                    print("Edit button was tapped")
-                }
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
-
-
-                HStack {
-                    Button {
-                        showingAlert2 = true
-                    } label: {
-                        Label("Edit", systemImage: "pencil")
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background(.red)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
                     }
-                    Button("Show Alert") {
-                        showingAlert = true
-                    }
-                    .alert("Important message1", isPresented: $showingAlert) {
-                        Button("OK", role: .destructive) { }
-                    } message: {
-                        Text("Please read this.")
-                    }
-                    .alert("Important message2", isPresented: $showingAlert2) {
-                        Button("OK", role: .cancel) { }
-                    } message: {
-                        Text("Please read this.")
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+
+                Spacer()
             }
+            .padding()
         }
-        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showingCurrentScore) {
+            Button("Continue", action: nextQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert("Game over!", isPresented: $showingFinalScore) {
+            Button("Continue", action: reset)
+        } message: {
+            Text("Your final score is \(score)")
+        }
+    }
 
-        Text(a.formatted())
+    private func flagTapped(_ answer: Int) {
+        if answer == correctAnswer {
+            scoreTitle = "You are right!"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That’s the flag of \(countries[answer])"
+        }
+  
+        showingCurrentScore = true
+        attemptCount += 1
+    }
+
+    private func nextQuestion() {
+        if attemptCount < maxAttemptCount {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        } else {
+            showingFinalScore = true
+        }
+    }
+
+    private func reset() {
+        attemptCount = 0
+        score = 0
+
+        nextQuestion()
     }
 }
 
